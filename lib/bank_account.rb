41d358452b22c8badd4336c3fb7c126a016d480f
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-01 21:46:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
 
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def valid?
+  status = "open" && balance > 0
+end
 end
