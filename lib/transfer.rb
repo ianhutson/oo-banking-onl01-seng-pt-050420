d003513c875350f682322f30b72ce97473e18cd8
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-01 21:47:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,13 @@ class Transfer
   
    def reverse_transaction
   if valid? && sender.balance > amount && self.status == "reversed"
+     sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+     reject_transfer
+    end
+  end
   def reject_transfer
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
